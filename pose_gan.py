a47edc8778a72869dd5296bbf8e8a495c0516507
Aliaksandr <aliaksandr.siarohin@gmail.com>
2017-09-15 15:19:57 +0200

    Move gan to submodule.

--- pose_gan.py
+++ pose_gan.py
@@ -13,16 +13,16 @@ class POSE_GAN(WGAN_GP):
         self._pose_estimator = pose_estimator
         
         self._set_trainable(self._pose_estimator, False)
-        self.generator_metric_names = []#['struct', 'gan']
+        self.generator_metric_names = ['struct', 'gan']
     
     def _compile_generator_loss(self):
-#         estimated_pose = self._pose_estimator (self._discriminator_fake_input[0][..., ::-1] / 2) [1]
-#         pose_loss = self._pose_penalty_weight * K.mean((estimated_pose[..., :18] -  self._generator_input[1]) ** 2)
+        estimated_pose = self._pose_estimator (self._discriminator_fake_input[0][..., ::-1] / 2) [1]
+        pose_loss = self._pose_penalty_weight * K.mean((estimated_pose[..., :18] -  self._generator_input[1]) ** 2)
         def pose_loss_fn(y_true, y_pred):
             return pose_loss
         def wasenstein_loss_fn(y_true, y_pred):
             return -K.mean(y_pred)
         def generator_loss_fn(y_true, y_pred):
-            return wasenstein_loss_fn(y_true, y_pred)# + pose_loss_fn(y_true, y_pred)
+            return wasenstein_loss_fn(y_true, y_pred) + pose_loss_fn(y_true, y_pred)
 
-        return generator_loss_fn,[]#, [pose_loss_fn, wasenstein_loss_fn]
+        return generator_loss_fn, [pose_loss_fn, wasenstein_loss_fn]
