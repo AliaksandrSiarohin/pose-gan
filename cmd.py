904a591671a89a51efb40e22ad58f4ffffb8bd4d
Aliaksandr <aliaksandr.siarohin@gmail.com>
2017-11-07 21:51:21 +0100

    Fix merge error.

--- cmd.py
+++ cmd.py
@@ -27,6 +27,7 @@ def args():
     parser.add_argument("--generator_checkpoint", default=None, help="Previosly saved model of generator")
     parser.add_argument("--discriminator_checkpoint", default=None, help="Previosly saved model of discriminator")
     parser.add_argument("--nn_loss_area_size", default=1, type=int, help="Use nearest neighbour loss")
+    parser.add_argument("--use_validation", default=1, type=int, help="Use validation")
 
     parser.add_argument('--dataset', default='market', choices=['market', 'fasion', 'fasion128'],
                         help='Market or fasion')
