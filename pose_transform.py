99d2eb4b73e25171ca4f0f89f9c3bda5cb5244d3
aliaksandr siarohin <aliaksandr.siarohin@gmail.com>
2017-11-10 13:35:13 +0100

    Fix bug in pose transfrom.

--- pose_transform.py
+++ pose_transform.py
@@ -128,45 +128,75 @@ def pose_masks(array2, img_size):
     st2 = compute_st_distance(kp2)
     empty_mask = np.zeros(img_size)
 
-    body_mask = mask_from_kp_array(get_array_of_points(kp2, ['Rhip', 'Lhip', 'Lsho', 'Rsho']), 0.1 * st2, img_size)
+    body_mask = np.ones(img_size)# mask_from_kp_array(get_array_of_points(kp2, ['Rhip', 'Lhip', 'Lsho', 'Rsho']), 0.1 * st2, img_size)
     masks.append(body_mask)
 
-    #head present
-    if 'nose' in kp2:
-        head_mask = mask_from_kp_array(get_array_of_points(kp2, ['nose']), 0.30 * st2, img_size)
+    head_candidate_names = {'Leye', 'Reye', 'Lear', 'Rear', 'nose'}
+    head_kp_names = set()
+    for cn in head_candidate_names:
+        if cn in kp2:
+            head_kp_names.add(cn)
+
+
+    if len(head_kp_names)!=0:
+        center_of_mass = np.mean(get_array_of_points(kp2, list(head_kp_names)), axis=0, keepdims=True)
+        center_of_mass = center_of_mass.astype(int)
+        head_mask = mask_from_kp_array(center_of_mass, 0.40 * st2, img_size)
         masks.append(head_mask)
     else:
         masks.append(empty_mask)
 
-    def mask_joint(fr, to):
+    def mask_joint(fr, to, inc_to):
         if not check_keypoints_present(kp2, [fr, to]):
             return empty_mask
-        return mask_from_kp_array(get_array_of_points(kp2, [fr, to]), 0.2 * st2, img_size)
+        return skimage.measure.grid_points_in_poly(img_size, estimate_polygon(kp2[fr], kp2[to], st2, inc_to, 0.1, 0.2, 0.2)[:, ::-1])
 
-    masks.append(mask_joint('Rkne', 'Rhip'))
-    masks.append(mask_joint('Lkne', 'Lhip'))
+    masks.append(mask_joint('Rhip', 'Rkne', 0.1))
+    masks.append(mask_joint('Lhip', 'Lkne', 0.1))
 
-    masks.append(mask_joint('Rank', 'Rkne'))
-    masks.append(mask_joint('Lank', 'Lkne'))
+    masks.append(mask_joint('Rkne', 'Rank', 0.5))
+    masks.append(mask_joint('Lkne', 'Lank', 0.5))
 
-    masks.append(mask_joint('Relb', 'Rsho'))
-    masks.append(mask_joint('Lelb', 'Lsho'))
+    masks.append(mask_joint('Rsho', 'Relb', 0.1))
+    masks.append(mask_joint('Lsho', 'Lelb', 0.1))
 
-    masks.append(mask_joint('Rwri', 'Relb'))
-    masks.append(mask_joint('Lwri', 'Lelb'))
+    masks.append(mask_joint('Relb', 'Rwri', 0.5))
+    masks.append(mask_joint('Lelb', 'Lwri', 0.5))
 
     return np.array(masks)
 
+
+def estimate_polygon(fr, to, st, inc_to, inc_from, p_to, p_from):
+    fr = fr + (fr - to) * inc_from
+    to = to + (to - fr) * inc_to
+
+    norm_vec = fr - to
+    norm_vec = np.array([-norm_vec[1], norm_vec[0]])
+    norm_vec = norm_vec / np.linalg.norm(norm_vec)
+
+    vetexes = np.array([
+        fr + st * p_from * norm_vec,
+        fr - st * p_from * norm_vec,
+        to - st * p_to * norm_vec,
+        to + st * p_to * norm_vec
+    ])
+
+    return vetexes
 
 def affine_transforms(array1, array2, img):
     kp1 = give_name_to_keypoints(array1)
     kp2 = give_name_to_keypoints(array2)
 
+    st1 = compute_st_distance(kp1)
+    st2 = compute_st_distance(kp2)
+
+
     no_point_tr = np.array([[1, 0, 1000], [0, 1, 1000], [0, 0, 1]])
 
     transforms = []
     def to_transforms(tr):
-        if np.linalg.cond(tr) < 10000:
+        print (np.linalg.cond(tr))
+        if np.linalg.cond(tr) < 1e100:
             transforms.append(tr)
         else:
             transforms.append(no_point_tr)
@@ -174,9 +204,6 @@ def affine_transforms(array1, array2, img):
     body_poly_1 = get_array_of_points(kp1, ['Rhip', 'Lhip', 'Lsho', 'Rsho'])
     body_poly_2 = get_array_of_points(kp2, ['Rhip', 'Lhip', 'Lsho', 'Rsho'])
     tr = skimage.transform.estimate_transform('affine', src=body_poly_2, dst=body_poly_1)
-    from skimage.transform import warp
-    from skimage import img_as_uint
-    plt.imshow(warp(img, tr.inverse).astype(np.uint8))
 
     to_transforms(tr.params)
 
@@ -186,48 +213,46 @@ def affine_transforms(array1, array2, img):
         if cn in kp1 and cn in kp2:
             head_kp_names.add(cn)
     if len(head_kp_names) != 0:
-        if len(head_kp_names) < 3:
-            head_kp_names.add('Lsho')
-            head_kp_names.add('Rsho')
+        #if len(head_kp_names) < 3:
+        head_kp_names.add('Lsho')
+        head_kp_names.add('Rsho')
         head_poly_1 = get_array_of_points(kp1, list(head_kp_names))
         head_poly_2 = get_array_of_points(kp2, list(head_kp_names))
-        tr = skimage.transform.estimate_transform('affine', dst=head_poly_2, src=head_poly_1)
+        tr = skimage.transform.estimate_transform('affine', src=head_poly_2, dst=head_poly_1)
         to_transforms(tr.params)
     else:
         to_transforms(no_point_tr)
 
-    def estimate_join(fr, to, anchor):
+    def estimate_join(fr, to, inc_to):
         if not check_keypoints_present(kp2, [fr, to]):
             return no_point_tr
-        poly_2 = get_array_of_points(kp2, [fr, to, anchor])
+        poly_2 = estimate_polygon(kp2[fr], kp2[to], st2, inc_to, 0.1, 0.2, 0.2)
         if check_keypoints_present(kp1, [fr, to]):
-            poly_1 = get_array_of_points(kp1, [fr, to, anchor])
+            poly_1 = estimate_polygon(kp1[fr], kp1[to], st1, inc_to, 0.1, 0.2, 0.2)
         else:
             if fr[0]=='R':
                 fr = fr.replace('R', 'L')
                 to = to.replace('R', 'L')
-                anchor = anchor.replace('R', 'L')
             else:
                 fr = fr.replace('L', 'R')
                 to = to.replace('L', 'R')
-                anchor = anchor.replace('L', 'R')
             if check_keypoints_present(kp1, [fr, to]):
-                poly_1 = get_array_of_points(kp1, [fr, to, anchor])
+                poly_1 = estimate_polygon(kp1[fr], kp1[to], st1, inc_to, 0.1, 0.2, 0.2)
             else:
                 return no_point_tr
         return skimage.transform.estimate_transform('affine', dst=poly_1, src=poly_2).params
 
-    to_transforms(estimate_join('Rhip', 'Rkne', 'Rsho'))
-    to_transforms(estimate_join('Lhip', 'Lkne', 'Lsho'))
+    to_transforms(estimate_join('Rhip', 'Rkne', 0.1))
+    to_transforms(estimate_join('Lhip', 'Lkne', 0.1))
 
-    to_transforms(estimate_join('Rkne', 'Rank', 'Rsho'))
-    to_transforms(estimate_join('Lkne', 'Lank', 'Lsho'))
+    to_transforms(estimate_join('Rkne', 'Rank', 0.3))
+    to_transforms(estimate_join('Lkne', 'Lank', 0.3))
 
-    to_transforms(estimate_join('Rsho', 'Relb', 'Rhip'))
-    to_transforms(estimate_join('Lsho', 'Lelb', 'Lhip'))
+    to_transforms(estimate_join('Rsho', 'Relb', 0.1))
+    to_transforms(estimate_join('Lsho', 'Lelb', 0.1))
 
-    to_transforms(estimate_join('Relb', 'Rwri', 'Rsho'))
-    to_transforms(estimate_join('Lelb', 'Lwri', 'Lsho'))
+    to_transforms(estimate_join('Relb', 'Rwri', 0.3))
+    to_transforms(estimate_join('Lelb', 'Lwri', 0.3))
 
     return np.array(transforms).reshape((-1, 9))[..., :-1]
 
@@ -296,16 +321,14 @@ if __name__ == "__main__":
         m = resize(m, (256, 256), preserve_range=True).astype(bool)
         fr_img = resize(fr_img, (256, 256), preserve_range=True).astype(float)
 
-        tr = affine_transforms(kp_fr, kp_to, fr_img)
-        masks = pose_masks(kp_to, fr_img.shape[:2])
-
-        print ([np.sum(mask) for mask in masks])
+        tr = affine_transforms(kp_fr, kp_to, fr_img)[1:]
+        masks = pose_masks(kp_to, fr_img.shape[:2])[1:]
 
         x = Input(fr_img.shape)
-        i = Input((10, 8))
-        mm = Input([10] + list(fr_img.shape[:2]))
+        i = Input((9, 8))
+        mm = Input([9] + list(fr_img.shape[:2]))
 
-        y = AffineTransformLayer(10, 'none', (256, 256))([x, i, mm])
+        y = AffineTransformLayer(9, 'max', (256, 256))([x, i, mm])
         model = Model(inputs=[x, i, mm], outputs=y)
 
         b = model.predict([fr_img[np.newaxis], tr[np.newaxis], masks[np.newaxis]])
